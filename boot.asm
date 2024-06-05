bc75aeda9952b1fa054a02adf0afb51cb53fd1f0
kerenskybr <roger.o.monteiro@gmail.com>
2024-06-05 09:03:12 -0300

    Writing to disk using bios

--- boot.asm
+++ boot.asm
@@ -10,12 +10,24 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
-handle_zero:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret 
+; DISK - READ SECTOR(S) INTO MEMORY
+; AH = 02h
+; AL = number of sectors to read (must be nonzero)
+; CH = low eight bits of cylinder number
+; CL = sector number 1-63 (bits 0-5)
+; high two bits of cylinder (bits 6-7, hard disk only)
+; DH = head number
+; DL = drive number (bit 7 set for hard disk)
+; ES:BX -> data buffer
+
+; Return:
+; CF set on error
+; if AH = 11h (corrected ECC error), AL = burst length
+; CF clear if successful
+; AH = status (see #00234)
+; AL = number of sectors transferred (only valid if CF set for some
+; BIOSes)
+
 
 step2:
     cli                     ; Clear interruptions
@@ -25,14 +37,27 @@ step2:
     mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
-    sti                             ; enable interruptions
+    sti                     ; enable interruptions
 
-    mov word[ss:0x00], handle_zero  ; zero segment
-    mov word[ss:0x02], 0x7c0
-    mov si, message                 ; move message to si address
+    ; Writing operations
+    mov ah, 2               ; REad sector command
+    mov al, 1               ; One sector to read
+    mov ch, 0               ; Cylinder low eight bits
+    mov cl, 2               ; Read sector 2
+    mov dh, 0               ; Head number
+    mov bx, buffer
+    int 0x13
+    jc error
+
+    mov si, buffer
     call print
+
     jmp $
 
+error:
+    mov si, error_message
+    call print
+    jmp $
 
 print:
     mov bx, 0
@@ -54,10 +79,13 @@ print_char:
     int 0x10                ;bios routine
     ret
 
-message : db 'Hello World!', 0
+error_message: db 'Failed to load sector'
 
 times 510-($ - $$) db 0     ; fill at least 510 bytes of data. Otherwise, will output zeros after the hello world thing
 dw 0xAA55                   ; bootloades sector
+
+; 7E00
+buffer: 
 
 
 ; https://wiki.osdev.org/
