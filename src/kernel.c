73e37ee08a5db79d6f2c91ae9630a8ecc7184276
kerenskybr <roger.o.monteiro@gmail.com>
2024-06-18 11:07:54 -0300

    successfully implemented paging

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 
 uint16_t* video_mem = 0; //(uint16_t*)(0xB8000);
 uint16_t terminal_row = 0;
@@ -69,6 +70,7 @@ void print(const char* str){
 
 //extern void problem();
 
+static struct paging_4gb_chunk* kernel_chunk = 0;
 void kernel_main(){
     
     terminal_initialize();
@@ -79,6 +81,14 @@ void kernel_main(){
 
     //Initialize the interrup
     idt_init();
+
+    // Setup paging
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+
+    //Switch to kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    enable_paging();
 
     // Enable interrupts
     enable_interrupts();
