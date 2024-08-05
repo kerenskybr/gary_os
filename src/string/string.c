6290307163fdc4653eea3dd43cde069b35ce6f23
kerenskybr <roger.o.monteiro@gmail.com>
2024-08-05 10:40:04 -0300

    finnaly found the stupid bug

--- src/string/string.c
+++ src/string/string.c
@@ -1,19 +1,17 @@
 #include "string.h"
 
-// https://www.asciitable.com/
-
-char tolower(char s1){
-
-    if (s1 >= 65 && s1 <= 90){ //ASCII capital letters, 65 - 90
-
-        s1 += 32; // plus 32 will convert to lower case (i.e. 65 + 32 = 97 (a))
+char tolower(char s1)
+{
+    if (s1 >= 65 && s1 <= 90)
+    {
+        s1 += 32;
     }
 
     return s1;
 }
 
-int strlen(const char* ptr){
-
+int strlen(const char* ptr)
+{
     int i = 0;
     while(*ptr != 0)
     {
@@ -24,8 +22,8 @@ int strlen(const char* ptr){
     return i;
 }
 
-int strnlen(const char* ptr, int max){
-
+int strnlen(const char* ptr, int max)
+{
     int i = 0;
     for (i = 0; i < max; i++)
     {
@@ -36,15 +34,13 @@ int strnlen(const char* ptr, int max){
     return i;
 }
 
-int strnlen_terminator(const char* str, int max, char terminator){
-
+int strnlen_terminator(const char* str, int max, char terminator)
+{
     int i = 0;
-    for (i = 0; i < max; i++){
-
-        if (str[i] == '\0' || str[i] == terminator){
-
+    for(i = 0; i < max; i++)
+    {
+        if (str[i] == '\0' || str[i] == terminator)
             break;
-        }
     }
 
     return i;
@@ -65,33 +61,28 @@ int istrncmp(const char* s1, const char* s2, int n)
 
     return 0;
 }
-
-int strncmp(const char* str1, const char* str2, int n){
-
+int strncmp(const char* str1, const char* str2, int n)
+{
     unsigned char u1, u2;
 
-    while(n-- > 0){
-
+    while(n-- > 0)
+    {
         u1 = (unsigned char)*str1++;
         u2 = (unsigned char)*str2++;
-
-        if (u1 != u2){
-
+        if (u1 != u2)
             return u1 - u2;
-        }
-        if (u1 == '\0'){
-
+        if (u1 == '\0')
             return 0;
-        }
     }
+
     return 0;
 }
 
-char* strcpy(char* dest, const char* src){
-
-    char* tmp = dest;
-    while(*src != 0){
-
+char* strcpy(char* dest, const char* src)
+{
+    char* res = dest;
+    while(*src != 0)
+    {
         *dest = *src;
         src += 1;
         dest += 1;
@@ -99,17 +90,17 @@ char* strcpy(char* dest, const char* src){
 
     *dest = 0x00;
 
-    return tmp;
+    return res;
 }
 
-char* strncpy(char* dest, const char* src, int count){
-    
+char* strncpy(char* dest, const char* src, int count)
+{
     int i = 0;
-    for (int i = 0; i < count-1; i++){
-
-        if (src[i] == 0x00){
+    for (i = 0; i < count-1; i++)
+    {
+        if (src[i] == 0x00)
             break;
-        }
+
         dest[i] = src[i];
     }
 
@@ -117,12 +108,11 @@ char* strncpy(char* dest, const char* src, int count){
     return dest;
 }
 
-bool isdigit(char c){
-
+bool isdigit(char c)
+{
     return c >= 48 && c <= 57;
 }
-
-int tonumericdigit(char c){
-    
+int tonumericdigit(char c)
+{
     return c - 48;
 }
