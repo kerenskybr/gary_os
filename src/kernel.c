e089ca6a354d95539c297f7dd36b538f5dbcc467
kerenskybr <roger.o.monteiro@gmail.com>
2024-08-07 10:08:36 -0300

    implemented virtual keyboard layer

--- src/kernel.c
+++ src/kernel.c
@@ -18,6 +18,8 @@
 #include "task/process.h"
 #include "status.h"
 #include "isr80h/isr80h.h"
+#include "keyboard/keyboard.h"
+
 uint16_t* video_mem = 0; //(uint16_t*)(0xB8000);
 uint16_t terminal_row = 0;
 uint16_t terminal_col = 0;
@@ -151,6 +153,8 @@ void kernel_main(){
 
     // register kernel commands
     isr80h_register_commands();
+
+    keyboard_init();
     
     struct process* process = 0;
     int res = process_load("0:/blank.bin", &process);
