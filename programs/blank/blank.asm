7ccf0544f31a18a100e7056be342c70186d22e9d
kerenskybr <roger.o.monteiro@gmail.com>
2024-08-27 13:59:05 -0300

    i can type now babY!

--- programs/blank/blank.asm
+++ programs/blank/blank.asm
@@ -4,14 +4,17 @@ section .asm
 
 global _start
 
+
 _start:
+
+_loop:
     call getkey
-    push message
-    mov eax, 1
+    push eax
+    mov eax, 3 ; 3 is command put char
     int 0x80
     add esp, 4
 
-    jmp $
+    jmp _loop
 
 getkey:
     mov eax, 2 ; comand getkey
