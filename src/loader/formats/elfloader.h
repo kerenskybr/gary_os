7587d88e35b8e2a8545d8fb67b92f8779155b3d9
kerenskybr <roger.o.monteiro@gmail.com>
2024-09-03 15:46:08 -0300

    implementing elf loader. Log road ahead

---
+++ src/loader/formats/elfloader.h
@@ -0,0 +1,29 @@
+#ifndef ELFLOADER_H
+#define ELFLOADER_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "elf.h"
+#include "config.h"
+
+struct elf_file{
+
+    char filename[GARYOS_MAX_PATH];
+
+    int in_memory_size;
+
+    // The physical mem addr that this elf file is loaded at
+    void* elf_memory;
+
+    // The virtual base addr of this binary
+    void* virtual_base_address;
+
+    void* virtual_end_address;
+
+    void* physical_base_address;
+    
+    void* physical_end_address;
+};
+
+#endif
