d56cfd0ef4534bd3b88c75652641add3ee52ebc4
kerenskybr <roger.o.monteiro@gmail.com>
2024-10-23 14:55:29 -0300

    starting stdlib user side

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,9 @@
+#include "garyos.h"
+
 int main(int argc, char** argv){
 
+    print("Hello stupid world! Ended yet?\n");
+
+    while(1) {}
     return 0;
 }
---
+++ programs/stdlib/src/garyos.h
@@ -0,0 +1,6 @@
+#ifndef GARYOS_H
+#define GARYOS_H
+
+void print(const char* message);
+
+#endif
