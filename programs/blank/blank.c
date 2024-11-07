94548fd61f4de1c5adc9555931532422d80ddca7
kerenskybr <roger.o.monteiro@gmail.com>
2024-11-07 09:05:26 -0300

    getkey wornking

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -4,6 +4,12 @@ int main(int argc, char** argv){
 
     print("Hello stupid world! Ended yet?\n");
 
-    while(1) {}
+    while(1) {
+
+        if (getkey() != 0){
+
+            print("Key was pressed\n");
+        }
+    }
     return 0;
 }
